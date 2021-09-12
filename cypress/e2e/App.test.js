f6152d444b1a0cfdf5efff5e93cd99d202536bbd
kjj6198 <kjj6198@gmail.com>
2021-09-13 02:43:56 +0900

    add cypress and e2e test

--- cypress/e2e/App.test.js
+++ cypress/e2e/App.test.js
@@ -1,6 +1,56 @@
-describe('fuck', () => {
-  it('should be OK', () => {
+import { data } from '../data'
+describe('App', () => {
+  beforeEach(() => {
+    // 攔截 GitHub API，套用測試用的假資料
+    cy.intercept('https://api.github.com/search/*', {
+      ...data
+    }).as('githubAPI')
+    cy.visit('/')
+  })
 
-    cy.go('/')
+  it('The slider component should exist', () => {
+    cy.findByRole('slider').should('exist')
+  })
+
+  it('should change value when being clicked', () => {
+    const defaultPerPage = 5
+    cy.findByText(/列表數/).should('contains.text', defaultPerPage)
+    cy.findByRole('slider').trigger('mousedown', 'right')
+    cy.findByText(/列表數/).should('contains.text', '30')
+  })
+
+  it('should change value when being dragged', () => {
+    cy.findByRole('slider')
+      .trigger('mousedown', 'left')
+      .wait(50)
+      .trigger('mousemove', 'right')
+      .wait(50)
+      .trigger('mouseleave')
+    cy.get('[role="slider"]')
+      .should('have.attr', 'aria-valuenow', '30')
+
+    cy.findByText(/列表數/).should('contains.text', '30')
+  })
+
+  it('should display API result correctly', () => {
+    // 使用 intercept 時可使用標籤讓 cypress 等待此 API 完成後再繼續執行
+    cy.wait('@githubAPI')
+    data.items.forEach((item) => {
+      cy.findByText(item.full_name).should('exist')
+    })
+  })
+})
+
+describe('App#error', () => {
+  it('should display error message when API error', () => {
+    cy.intercept('GET', 'https://api.github.com/search/*', {
+      statusCode: 429,
+      body: {
+        error: 'You called too many times'
+      }
+    }).as('error')
+    cy.visit('/')
+    cy.wait('@error')
+    cy.findByText('err: Too Many Requests').should('exist')
   })
 })
